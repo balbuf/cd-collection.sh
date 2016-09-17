3730e2d2045f01f626fd6a2329f1c210ecb3c550
Stephen Beemsterboer <balbuf@gmail.com>
2016-09-17 10:46:42 -0400

    Add "show" and "add" commands

--- bookmarks.sh
+++ bookmarks.sh
@@ -12,17 +12,49 @@ if [[ -z "$@" ]]; then
 	return 1
 fi
 
+# test the symlink name when adding a new one
+function test_name() {
+	# symlink name cannot contain a slash
+	if [[ "$1" == *\/* ]]; then
+		echo "Error: Bookmark name cannot contain a forward slash." >&2
+		return 2
+	fi
+}
+
+# does the target exist?
+function test_target() {
+	if [[ ! -d "$dir/$1" ]]; then
+		echo "Error: Target '$1' does not exist." >&2
+		return 2
+	fi
+}
+
+# what command are we running?
 case "$1" in
 	# create a new bookmark or overwrite existing
 	'set')
-		# symlink name cannot contain a slash
-		if [[ "$2" == *\/* ]]; then
-			echo "Bookmark name cannot contain a forward slash" >&2
-			return 2
-		fi
+		test_name "$2" || return $?
 		# attempt to create the symlink
 		ln -sfn "$PWD" "$dir/$2" || return $?
+		echo "Success: Set '$2' pointing to $PWD"
+	;;
+
+	# create a new bookmark without overwriting
+	'add')
+		test_name "$2" || return $?
+		# does the target already exist?
+		if test_target "$2" &> /dev/null; then
+			echo "Error: Target '$2' already exists." >&2
+			return 2
+		fi
+		ln -s "$PWD" "$dir/$2" || return $?
 		echo "Success: Added '$2' pointing to $PWD"
+	;;
+
+	# show the destination of an existing bookmark
+	'show')
+		test_target "$2" || return $?
+		readlink "$dir/$2"
 	;;
 
 	# explicitly go to a bookmark, in case a reserved word is used
@@ -40,12 +72,7 @@ esac
 [[ -z "$bm" ]] && return 0
 
 # go to the destination, if it exists
-target="$dir/$bm"
-# does the target exist?
-if [[ ! -d "$target" ]]; then
-	echo "Target '$bm' does not exist." >&2
-	return 2
-fi
+test_target "$bm" || return $?
 
 # go to the physical (-P) location that the symlink points to
-cd -P "$target"
+cd -P "$dir/$bm"
