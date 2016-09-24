79c191d010d64294cf0ff1f4c769ed2cbda9f67e
Stephen Beemsterboer <balbuf@gmail.com>
2016-09-24 00:38:19 -0400

    Lots of restructuring and renaming

--- bookmarks.sh
+++ bookmarks.sh
@@ -1,124 +1,125 @@
 #!/bin/sh
 
-# use parens instead of braces to allow interal helper functions
-function bm() (
-
-# directory to store bookmarks - defaults to home dir
-local dir="${BOOKMARK_DIR:-$HOME/.bookmarks}"
-local target=''
-mkdir -p "$dir"
+function cdc() {
 
 # basic usage instructions
-if [[ -z "$@" ]]; then
+if [[ -z "$@" || "$1" == 'help' ]]; then
 	cat <<-'DOG'
-		usage: bm [--] <target>
-		       bm <command> [<args>]
+		usage: cdc [--] <target>
+		       cdc <command> [<args>]
 
 		COMMANDS
 
-		   add    Add a new bookmark without overwriting an existing one
+		   add    Add a new target without overwriting an existing one
+		   get    Get the path of a given target
 		   go     Go to the given target
 		   help   Display this message
-		   ls     List all existing bookmarks
-		   set    Add or overwrite an existing bookmark
-		   show   Show the target path of a given bookmark
-		   rm     Remove an existing bookmark
+		   ls     List all existing targets
+		   set    Add or overwrite an existing target
+		   rm     Remove an existing target
 
 	DOG
-	return 1
+	# test whether we ran the help command to determine what exit status to use
+	[[ "$1" == 'help' ]]
+	return $?
 fi
 
-# test the symlink name when adding a new one
-function test_name() {
-	# symlink name cannot contain a slash
-	if [[ "$1" == *\/* ]]; then
-		echo "Error: Bookmark name cannot contain a forward slash." >&2
-		return 2
-	fi
-}
-
-# does the target exist?
-function test_target() {
-	if [[ ! -d "$dir/$1" ]]; then
-		echo "Error: Target '$1' does not exist." >&2
-		return 2
-	fi
-}
+# directory to store bookmarks - defaults to home dir
+local dir="${CD_COLLECTION:-$HOME/.cdcollection}"
+local command="$1"
 
 # what command are we running?
-case "$1" in
+case "$command" in
 	# create a new bookmark or overwrite existing
-	'set')
-		test_name "$2" || return $?
-		# attempt to create the symlink
-		ln -sfn "$PWD" "$dir/$2" || return $?
-		echo "Success: Set '$2' pointing to $PWD"
-	;;
-
-	# create a new bookmark without overwriting
-	'add')
-		test_name "$2" || return $?
-		# does the target already exist?
-		if test_target "$2" &> /dev/null; then
-			echo "Error: Target '$2' already exists." >&2
+	'set' | 'add')
+		# check the name format
+		if [[ "$2" == *\/* ]]; then
+			echo 'Error: Target name cannot contain a forward slash.' >&2
 			return 2
 		fi
-		ln -s "$PWD" "$dir/$2" || return $?
-		echo "Success: Added '$2' pointing to $PWD"
+
+		# make sure dir exists
+		mkdir -p "$dir"
+
+		# attempt to add or overwrite the symlink
+		if [[ "$command" == 'set' ]]; then
+			ln -sfn "$PWD" "$dir/$2" || return $?
+			echo "Success: Set '$2' pointing to $PWD"
+		# otherwise check if it already exists
+		elif [[ -L "$dir/$2" ]]; then
+			echo "Error: Target '$2' already exists." >&2
+			return 2
+		# otherwise add the symlink
+		else
+			ln -s "$PWD" "$dir/$2" || return $?
+			echo "Success: Added '$2' pointing to $PWD"
+		fi
 	;;
 
-	# show the destination of an existing bookmark
-	'show')
-		test_target "$2" || return $?
-		readlink "$dir/$2"
-	;;
-
-	# remove an existing bookmark
+	# remove an existing target
 	'rm' | 'remove')
-		# does it exist?
-		test_target "$2" || return $?
-		# is it a subdir or file of the actual bookmark?
-		if ! test_name "$2" &> /dev/null; then
-			echo "Error: Target '$2' cannot be removed." >&2
+		# is it a subdir or file of the actual target?
+		if [[ "$2" == *\/* || ! -L "$dir/$2" ]]; then
+			echo "Error: '$2' is not a target that can be removed." >&2
 			return 2
 		fi
 		rm "$dir/$2" || return $?
-		echo "Success: Removed target '$2' pointing to $PWD"
+		echo "Success: Removed target '$2'"
 	;;
 
-	# explicitly go to a bookmark, in case a reserved word is used
-	'go' | '--')
-		target="$2"
-	;;
-
-	# list all of the bookmarks
+	# list all of the targets
 	'ls' | 'list')
 		local file
-		# iterate on the contents of the dir - use which in case ls is aliased
-		for file in $($(which ls) "$dir"); do
-			[[ -L "$dir/$file" && -d "$dir/$file" ]] || continue
-			echo "$file -> $(readlink "$dir/$file")"
+		# iterate on the contents of the dir
+		for file in "$dir/"*; do
+			# is it a symlink to a directory?
+			[[ -L "$file" && -d "$file" ]] || continue
+			echo "$(basename "$file") -> $(readlink "$file")"
 		done
 	;;
 
-	# display the help message
-	'help')
-		bm
-	;;
-
-	# default - assumes the arg is a bookmark
+	# everything else
 	*)
-		target="$1"
+		local target
+		# resolve the target
+		case "$command" in
+			# explicit commands
+			'go' | '--' | 'get')
+				if [[ -z "$2" ]]; then
+					echo "Error: No target specified." >&2
+					return 2
+				fi
+				target="$2"
+			;;
+
+			# implicit commands
+			*)
+				# if there is a second arg, then the command is not valid
+				if [[ -n "$2" ]]; then
+					echo "Error: Unknown command '$command'" >&2
+					return 2
+				fi
+				target="$1"
+				# if we are not in a terminal, implicit command is 'get'
+				if [[ ! -t 1 ]]; then
+					command='get'
+				fi
+			;;
+		esac
+		# does the target exist?
+		if [[ ! -d "$dir/$target" ]]; then
+			echo "Error: '$target' is not a valid target." >&2
+			return 2
+		fi
+		# explicitly go to a bookmark, in case a reserved word is used
+		if [[ "$command" == 'get' ]]; then
+			# print the target
+			echo "$(cd -P "$dir/$target" && pwd)"
+		else
+			# go to the physical (-P) location that the symlink points to
+			cd -P "$dir/$target"
+		fi
 	;;
 esac
 
-# no target? we have completed some other action successfully
-[[ -z "$target" ]] && return 0
-
-# test that the target even exists
-test_target "$target" || return $?
-
-# go to the physical (-P) location that the symlink points to
-cd -P "$dir/$target"
-
-) # end bm()
+} # end cdc()
